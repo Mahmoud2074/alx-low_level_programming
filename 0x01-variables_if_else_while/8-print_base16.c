d80a199a203acafb8062656f9ed03600e6599c1c
Mahmoud2074 <mah.ashraf60@gmail>
2023-08-11 17:12:08 -0700

    c lang

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char ch;
+	int num;
+	char letter;
 
-    for (ch = '0'; ch <= '9'; ch++)
-    {
-        putchar(ch);
-    }
-    for (ch = 'a'; ch <= 'f'; ch++)
-    {
-        putchar(ch);
-    }
-    putchar('\n');
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
 
-    return 0;
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
