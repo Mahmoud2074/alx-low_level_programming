0c2e45e1c8dab3ce9fabe7cab6188702f0050177
Mahmoud2074 <mah.ashraf60@gmail>
2023-08-11 18:43:40 -0700

    c lang

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: print a qoute using write function 
- *      write(int fd,const void *buf,size_t count)
+ * Description: print a qoute using write function
+ *     ssize_t  write(int fd,const void *buf,size_t count);
  *
  * Return: Always 0 (Success)
 */
@@ -13,4 +13,4 @@ int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
