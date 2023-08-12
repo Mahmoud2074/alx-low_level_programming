e371bd92981611d7cdd7a2f154f270f47ed79293
Mahmoud2074 <mah.ashraf60@gmail>
2023-08-11 18:09:46 -0700

    c lang

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: using sizeof to print the size of various types.
  *
  * Return: Always 0 (Success)
 */
